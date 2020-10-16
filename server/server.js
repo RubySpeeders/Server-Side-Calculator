08eda7469d26bf96a8af8dc0d75913b0ce0cb883
Sarah Peters <sarahnpeters@gmail.com>
2020-10-16 16:23:08 -0500

    adding functions for post to get object information. adding bodyParser information

--- server/server.js
+++ server/server.js
@@ -1,8 +1,22 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
+
 const PORT = process.env.PORT || 5000;
 
+let calcFields = [];
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+//Save Data
+app.post('/calculation', (req, res) => {
+  console.log(req.body);
+  res.sendStatus(200);
+});
+
 app.use(express.static('public'));
+
 app.listen(PORT, () => {
   console.log('Listening on PORT: ', PORT);
 });
