8c0f55996b061ee2e358c6267f222f3d77a34465
Sarah Peters <sarahnpeters@gmail.com>
2020-10-16 21:21:28 -0500

    adding toggle class for css on buttons

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -21,7 +21,7 @@ let operator = '';
 
 function chooseOperator() {
   operator = $(this).prop('name');
-  $(this).addClass('addColour');
+  $(this).toggleClass('addColour');
   return operator;
 }
 
