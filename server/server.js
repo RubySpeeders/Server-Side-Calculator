03580fc2dafd7540078d28d3f854ff87fa886209
Sarah Peters <sarahnpeters@gmail.com>
2020-10-16 20:12:24 -0500

    adding for loop in server.js to allocate which operator is clicked

--- server/server.js
+++ server/server.js
@@ -12,7 +12,21 @@ app.use(bodyParser.json());
 //POST from input fields
 //const obj = { numOne: 0, operation: '', numTwo: 0 };
 app.post('/calculation', (req, res) => {
-  console.log(req.body);
+  //console.log(req.body);
+  calcFields.push(req.body);
+  console.log(calcFields);
+  for (let i = 0; i < calcFields.length; i++) {
+    const entry = calcFields[i];
+    if (entry.operation === 'add') {
+      //console.log('add that bench');
+    } else if (entry.operation === 'subtract') {
+      //console.log('subtract that bench');
+    } else if (entry.operation === 'multiply') {
+      //console.log('multiply that bench');
+    } else if (entry.operation === 'divide') {
+      //console.log('divide that bench');
+    }
+  }
   res.sendStatus(200);
 });
 
